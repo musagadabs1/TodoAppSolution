291271d79f419d2668db7f94a06cbedc52f35f22
Musa Sule <musagadabs1@.com>
2018-10-17 16:20:51 +0100

    Add project files.

---
+++ TodoApp/Models/TodoViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApp.Models
+{
+    public class TodoViewModel
+    {
+        public TodoItem[] Items { get; set; }
+    }
+}
