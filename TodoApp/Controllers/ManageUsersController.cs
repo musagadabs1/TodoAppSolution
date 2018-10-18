382912ea45964780a7821ebbd0d61fb0eaf5c40a
Musa Sule <musagadabs1@.com>
2018-10-18 16:41:23 +0100

    Added UserManager

---
+++ TodoApp/Controllers/ManageUsersController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TodoApp.Models;
+
+namespace TodoApp.Controllers
+{
+    [Authorize(Roles="Administrator")]
+    public class ManageUsersController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ManageUsersController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
